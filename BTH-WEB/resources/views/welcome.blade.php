c8677935ba346cc691aff8bb8d291cbbe8c4f933
LeonardoSoliz <leonardopadilla466@gmail.com>
2020-04-02 00:45:51 +0100

    Aplicacion ADM-BTH

---
+++ BTH-WEB/resources/views/welcome.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="icon" type="image/png" href="https://cdn.saaspass.com/a52e2205866340ea/authenticators/gitlab_128.png"/>
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="../css/util.css">
+        <link rel="stylesheet" type="text/css" href="../css/main.css">
+
+
+        <!--===============================================================================================-->
+    </head>
+    <body>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <!-- <div class="container wrap-alert">
+                <br>
+                <div class="alert alert-danger">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    Hello
+                </div>
+            </div> -->
+
+            <div class="wrap-login100">
+
+
+                <form class="login100-form validate-form">
+					<span class="login100-form-title">
+						LOGIN <br>
+						<small style="font-weight: lighter; font-size: 14px; color: #777">BTH</small>
+					</span>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email" placeholder="Email">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                        <input class="input100" type="password" name="pass" placeholder="ContraseÃ±a">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Entrar
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-12">
+						<span class="txt1">
+							No Estas Registrado?
+						</span>
+                        <a class="txt2" href="Registro.html">
+                            Solicitar Cuenta.
+                        </a>
+                    </div>
+
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="#">
+                            &copy 2020 Softwarelll .
+                            <!-- <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i> -->
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--===============================================================================================-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js"></script>
+    <script >
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+
+    </body>
+</html>
+
+
