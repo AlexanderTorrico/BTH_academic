df6a6ecd3d12baa9ae840ba3d28777d354637160
Carlos Clavijo <carlo96cf@gmail.com>
2020-07-21 20:09:09 -0400

    Base de datos reestructurada

--- DataBase/2.1.0v.sql
+++ DataBase/2.1.0v.sql
@@ -1,42 +1,27 @@
-
 ALTER TABLE tblnotas DROP FOREIGN KEY tblnotas_ibfk_2;
-
-
 ALTER TABLE tblNotas ADD CONSTRAINT tblnotas_ibfk_2 FOREIGN KEY (idParametro_grupo) REFERENCES tblParametros(id);
-
-ALTER TABLE tblNotas modify column nota DOUBLE;
-
-
-alter table tblproyectos add estado boolean;
-
-alter table tblproyectos drop foreign key tblproyectos_ibfk_1;
+ALTER TABLE tblNotas MODIFY COLUMN nota DOUBLE;
+ALTER TABLE tblproyectos ADD estado BOOLEAN;
+ALTER TABLE tblproyectos DROP FOREIGN KEY tblproyectos_ibfk_1;
 
 DELIMITER $$
-
-DROP FUNCTION IF EXISTS fn_changePassword;
 CREATE FUNCTION fn_changePassword(_email varchar(50), _sha text, _tipo varchar(1), _pass text)
 RETURNS varchar(50)
 BEGIN
     declare v_date varchar(50);
     set v_date ="null";
-
     SELECT t.email  into v_date from tblTokens t
         WHERE t.email = _email and t.sha = _sha;
-
     if v_date != 'null' then
         if _tipo = 'c' then
             delete from tblTokens where email = v_date;
             UPDATE tblColegios
             SET contrasenia=hex(aes_encrypt(_pass,'COL')) WHERE correo=_email;
-        
         end if;
-
     end if;
-
     return v_date;
 END$$
 
-DROP procedure IF EXISTS sp_cursorInsertNote;
 CREATE procedure sp_cursorInsertNote(_tipo varchar(1), _trimestre int, _idGrupo int)
 BLOCK1: begin
 	declare ultimoParametro int default 0;
@@ -44,60 +29,43 @@ BLOCK1: begin
     declare no_more_rows1 boolean default FALSE;
     declare cursor1 cursor for select id from tblParametros p
 				where p.tipo = _tipo and p.trimestre = _trimestre and p.idGrupo = _idGrupo;
-
     declare continue handler for not found  
         set no_more_rows1 := TRUE;  
-
     open cursor1;
     LOOP1: loop
         fetch cursor1
         into  idParm;
-
         if no_more_rows1 then
             close cursor1;
             leave LOOP1;
         end if;
-
         BLOCK2: begin
             declare idEstGrup int;
             declare no_more_rows2 boolean default FALSE;
-           
             declare cursor2 cursor for select a.id from (select * from tblEstudiantes_grupos eg
 									where eg.idGrupo = _idGrupo) a
 						left join (select * from tblnotas n
 									where n.idParametro_grupo = idParm) b on a.id = b.idEstudiante_grupo
 						where b.id is null;
-
 			declare continue handler for not found
                set no_more_rows2 := TRUE;
-           
             open cursor2;
             LOOP2: loop
                 fetch cursor2
                 into  idEstGrup;
-
                 if ultimoParametro != idEstGrup then
-
                 	insert into tblNotas values(default,0,idEstGrup,idParm);
                 	set ultimoParametro := idEstGrup;
                 end if;
-
                 if no_more_rows2 then
                     close cursor2;
                     leave LOOP2;
                 end if;
-
             end loop LOOP2;
         end BLOCK2;
     end loop LOOP1;
 end BLOCK1;$$
 
-
-
-
-
-
-DROP PROCEDURE IF EXISTS sp_notasAnuales;
 CREATE PROCEDURE sp_notasAnuales(_idGrupo int)
 BEGIN  
 	select est.*, ifnull(nota.primer,0) as primer, ifnull(nota.segundo,0) as segundo, ifnull(nota.tercer,0) as tercero from (select e.id, concat(e.apaterno,' ',e.amaterno,' ',e.nombre) as nombre from tblEstudiantes e
@@ -140,6 +108,4 @@ BEGIN
 				join tblnotas n on n.idParametro_grupo = a.id
 				group by n.idEstudiante_grupo) c1 on a1.id = c1.id) z on x.id = z.id) nota on est.id = nota.id
 	order by nombre;
-
 END $$
-
--- DataBase/4.0.0.sql
+++ DataBase/4.0.0.sql
@@ -1,233 +1,186 @@
+
 -- Tabla Usuarios
-CREATE TABLE tblusuarios(
+CREATE TABLE tblUsuarios(
 	id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     apPaterno VARCHAR(50) NOT NULL,
     apMaterno VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
     password TEXT,
     estado INT,
     PRIMARY KEY(id)
 );
 
--- Tabla Roles
-CREATE TABLE tblroles(
+create table tblUsuariosRoles(
 	id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(25) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES tblusuarios(id)
+	idreference INT,
+	idUsuario INT NOT NULL,
+	idRol INT NOT NULL,
+	estado BOOLEAN,
+    PRIMARY KEY(id)
 );
 
-INSERT INTO tblroles VALUES(DEFAULT, 'Estudiante');
-INSERT INTO tblroles VALUES(DEFAULT, 'Docente');
-INSERT INTO tblroles VALUES(DEFAULT, 'Administrador');
-INSERT INTO tblroles VALUES(DEFAULT, 'AdministradorBTH');
-
-CREATE TABLE tblpermisos(
+-- Tabla Roles
+CREATE TABLE tblRoles(
 	id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(25) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE tblPermisos(
+	id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE tblrolespermisos(
+CREATE TABLE tblRolesPermisos(
 	idRol INT,
     idPermiso INT,
-    FOREIGN KEY (idRol) REFERENCES tblroles(id),
+    FOREIGN KEY (idRol) REFERENCES tblRoles(id),
     FOREIGN KEY (idPermiso) REFERENCES tblPermisos(id)
 );
 
-CREATE TABLE tblusuarioroles(
-	idUsuario INT,
-    idRol INT,
-    FOREIGN KEY (idUsuario) REFERENCES tblusuarios(id),
-    FOREIGN KEY (idRol) REFERENCES tblroles(id)
-);
+INSERT INTO tblRoles VALUES(DEFAULT, 'Docente');
+INSERT INTO tblRoles VALUES(DEFAULT, 'Administrador');
+INSERT INTO tblRoles VALUES(DEFAULT, 'AdministradorBTH');
 
+-- 1 AdministradorRoles --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdministradorRoles', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdministradorRoles', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdministradorRoles', 'EscrituraLectura');
 
--- Usuario
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertUsuario');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getUsuario');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateUsuario');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteUsuario');
+-- 2 ColegiosR --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ColegiosR', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ColegiosR', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ColegiosR', 'EscrituraLectura');
 
--- Carrera
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertCarrera');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getCarrera');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateCarrera');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteCarrera');
+-- 3 Gestiones --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Gestiones', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Gestiones', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Gestiones', 'EscrituraLectura');
 
--- Asistencia 
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertAsistencia');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getAsistencia');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateAsistencia');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteAsistencia');
+-- 4 Paginas Inicio --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PaginaInicio', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PaginaInicio', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PaginaInicio', 'EscrituraLectura');
 
--- Colegio
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertColegio');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getColegio');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateColegio');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteColegio');
+-- 5 Perfil Carrera --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilCarrera', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilCarrera', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilCarrera', 'EscrituraLectura');
 
--- Docente
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertDocente');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getDocente');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateDocente');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteDocente');
+-- 6 Perfil Colegio --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilColegio', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilColegio', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilColegio', 'EscrituraLectura');
 
--- Estudiante
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertEstudiante');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getEstudiante');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateEstudiante');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteEstudiante');
+-- 7 Perfil Estudiante --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilEstudiante', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilEstudiante', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'PerfilEstudiante', 'EscrituraLectura');
 
--- Gestion
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertGestion');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getGestion');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateGestion');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteGestion');
+-- 8 Reportes Notas --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesNotas', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesNotas', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesNotas', 'EscrituraLectura');
 
--- Grupos
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertGrupos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getGrupos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateGrupos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteGrupos');
+-- 9 Reportes Pagos --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesPagos', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesPagos', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ReportesPagos', 'EscrituraLectura');
 
--- Notas
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertNotas');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getNotas');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateNotas');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteNotas');
+-- 10 Administrador Proyectos --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdminProyecto', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdminProyecto', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AdminProyecto', 'EscrituraLectura');
 
--- Pagos
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertPagos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getPagos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updatePagos');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deletePagos');
+-- 11 Asistencia Docente --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AsistenciaDocente', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AsistenciaDocente', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'AsistenciaDocente', 'EscrituraLectura');
 
--- Proyecto
-INSERT INTO tblpermisos VALUES(DEFAULT, 'insertProyecto');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'getProyecto');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'updateProyecto');
-INSERT INTO tblpermisos VALUES(DEFAULT, 'deleteProyecto');
+-- 12 Base --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Base', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Base', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Base', 'EscrituraLectura');
 
--- Permisos de estudiantes
-INSERT INTO tblrolesspermisos VALUES(1, 2);
-INSERT INTO tblrolesspermisos VALUES(1, 3);
-INSERT INTO tblrolesspermisos VALUES(1, 6);
-INSERT INTO tblrolesspermisos VALUES(1, 10);
-INSERT INTO tblrolesspermisos VALUES(1, 14);
-INSERT INTO tblrolesspermisos VALUES(1, 18);
-INSERT INTO tblrolesspermisos VALUES(1, 21);
-INSERT INTO tblrolesspermisos VALUES(1, 22);
-INSERT INTO tblrolesspermisos VALUES(1, 23);
-INSERT INTO tblrolesspermisos VALUES(1, 24);
-INSERT INTO tblrolesspermisos VALUES(1, 26);
-INSERT INTO tblrolesspermisos VALUES(1, 30);
-INSERT INTO tblrolesspermisos VALUES(1, 34);
-INSERT INTO tblrolesspermisos VALUES(1, 38);
-INSERT INTO tblrolesspermisos VALUES(1, 42);
-INSERT INTO tblrolesspermisos VALUES(1, 46);
+-- 13 Carrera --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Carrera', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Carrera', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Carrera', 'EscrituraLectura');
 
-SELECT * FROM tblpermisos;
--- Permisos para docentes
-INSERT INTO tblrolesspermisos VALUES(2, 2);
-INSERT INTO tblrolesspermisos VALUES(2, 3);
-INSERT INTO tblrolesspermisos VALUES(2, 4);
-INSERT INTO tblrolesspermisos VALUES(2, 5);
-INSERT INTO tblrolesspermisos VALUES(2, 9);
-INSERT INTO tblrolesspermisos VALUES(2, 10);
-INSERT INTO tblrolesspermisos VALUES(2, 11);
-INSERT INTO tblrolesspermisos VALUES(2, 12);
-INSERT INTO tblrolesspermisos VALUES(2, 14);
-INSERT INTO tblrolesspermisos VALUES(2, 17);
-INSERT INTO tblrolesspermisos VALUES(2, 19);
-INSERT INTO tblrolesspermisos VALUES(2, 22);
-INSERT INTO tblrolesspermisos VALUES(2, 29);
-INSERT INTO tblrolesspermisos VALUES(2, 30);
-INSERT INTO tblrolesspermisos VALUES(2, 31);
-INSERT INTO tblrolesspermisos VALUES(2, 32);
-INSERT INTO tblrolesspermisos VALUES(2, 33);
-INSERT INTO tblrolesspermisos VALUES(2, 34);
-INSERT INTO tblrolesspermisos VALUES(2, 35);
-INSERT INTO tblrolesspermisos VALUES(2, 41);
-INSERT INTO tblrolesspermisos VALUES(2, 42);
-INSERT INTO tblrolesspermisos VALUES(2, 47);
+-- 14 Change Password
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ChangePassword', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ChangePassword', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'ChangePassword', 'EscrituraLectura');
 
-select * from tblpermisos;
--- Roles administrador
-INSERT INTO tblrolesspermisos VALUES(3, 2);
-INSERT INTO tblrolesspermisos VALUES(3, 3);
-INSERT INTO tblrolesspermisos VALUES(3, 4);
-INSERT INTO tblrolesspermisos VALUES(3, 5);
-INSERT INTO tblrolesspermisos VALUES(3, 6);
-INSERT INTO tblrolesspermisos VALUES(3, 7);
-INSERT INTO tblrolesspermisos VALUES(3, 8);
-INSERT INTO tblrolesspermisos VALUES(3, 12);
-INSERT INTO tblrolesspermisos VALUES(3, 13);
-INSERT INTO tblrolesspermisos VALUES(3, 14);
-INSERT INTO tblrolesspermisos VALUES(3, 15);
-INSERT INTO tblrolesspermisos VALUES(3, 16);
-INSERT INTO tblrolesspermisos VALUES(3, 17);
-INSERT INTO tblrolesspermisos VALUES(3, 18);
-INSERT INTO tblrolesspermisos VALUES(3, 19);
-INSERT INTO tblrolesspermisos VALUES(3, 20);
-INSERT INTO tblrolesspermisos VALUES(3, 21);
-INSERT INTO tblrolesspermisos VALUES(3, 22);
-INSERT INTO tblrolesspermisos VALUES(3, 23);
-INSERT INTO tblrolesspermisos VALUES(3, 24);
-INSERT INTO tblrolesspermisos VALUES(3, 25);
-INSERT INTO tblrolesspermisos VALUES(3, 26);
-INSERT INTO tblrolesspermisos VALUES(3, 27);
-INSERT INTO tblrolesspermisos VALUES(3, 28);
-INSERT INTO tblrolesspermisos VALUES(3, 32);
-INSERT INTO tblrolesspermisos VALUES(3, 36);
-INSERT INTO tblrolesspermisos VALUES(3, 37);
-INSERT INTO tblrolesspermisos VALUES(3, 38);
-INSERT INTO tblrolesspermisos VALUES(3, 39);
-INSERT INTO tblrolesspermisos VALUES(3, 40);
-INSERT INTO tblrolesspermisos VALUES(3, 44);
+-- 15 Colegios --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Colegios', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Colegios', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Colegios', 'EscrituraLectura');
 
--- Permisos Administrador BTH
-INSERT INTO tblrolesspermisos VALUES(4, 1);
-INSERT INTO tblrolesspermisos VALUES(4, 2);
-INSERT INTO tblrolesspermisos VALUES(4, 3);
-INSERT INTO tblrolesspermisos VALUES(4, 4);
-INSERT INTO tblrolesspermisos VALUES(4, 5);
-INSERT INTO tblrolesspermisos VALUES(4, 6);
-INSERT INTO tblrolesspermisos VALUES(4, 7);
-INSERT INTO tblrolesspermisos VALUES(4, 8);
-INSERT INTO tblrolesspermisos VALUES(4, 9);
-INSERT INTO tblrolesspermisos VALUES(4, 10);
-INSERT INTO tblrolesspermisos VALUES(4, 11);
-INSERT INTO tblrolesspermisos VALUES(4, 12);
-INSERT INTO tblrolesspermisos VALUES(4, 13);
-INSERT INTO tblrolesspermisos VALUES(4, 14);
-INSERT INTO tblrolesspermisos VALUES(4, 15);
-INSERT INTO tblrolesspermisos VALUES(4, 16);
-INSERT INTO tblrolesspermisos VALUES(4, 17);
-INSERT INTO tblrolesspermisos VALUES(4, 18);
-INSERT INTO tblrolesspermisos VALUES(4, 19);
-INSERT INTO tblrolesspermisos VALUES(4, 20);
-INSERT INTO tblrolesspermisos VALUES(4, 21);
-INSERT INTO tblrolesspermisos VALUES(4, 22);
-INSERT INTO tblrolesspermisos VALUES(4, 23);
-INSERT INTO tblrolesspermisos VALUES(4, 24);
-INSERT INTO tblrolesspermisos VALUES(4, 25);
-INSERT INTO tblrolesspermisos VALUES(4, 26);
-INSERT INTO tblrolesspermisos VALUES(4, 27);
-INSERT INTO tblrolesspermisos VALUES(4, 28);
-INSERT INTO tblrolesspermisos VALUES(4, 29);
-INSERT INTO tblrolesspermisos VALUES(4, 30);
-INSERT INTO tblrolesspermisos VALUES(4, 31);
-INSERT INTO tblrolesspermisos VALUES(4, 32);
-INSERT INTO tblrolesspermisos VALUES(4, 33);
-INSERT INTO tblrolesspermisos VALUES(4, 34);
-INSERT INTO tblrolesspermisos VALUES(4, 35);
-INSERT INTO tblrolesspermisos VALUES(4, 36);
-INSERT INTO tblrolesspermisos VALUES(4, 37);
-INSERT INTO tblrolesspermisos VALUES(4, 38);
-INSERT INTO tblrolesspermisos VALUES(4, 39);
-INSERT INTO tblrolesspermisos VALUES(4, 40);
-INSERT INTO tblrolesspermisos VALUES(4, 41);
-INSERT INTO tblrolesspermisos VALUES(4, 42);
-INSERT INTO tblrolesspermisos VALUES(4, 43);
-INSERT INTO tblrolesspermisos VALUES(4, 44);
+-- 16 Docente Grupos
+INSERT INTO tblPermisos VALUES(DEFAULT, 'DocenteGrupos', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'DocenteGrupos', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'DocenteGrupos', 'EscrituraLectura');
+
+-- 17 EmailSendCorreo --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'EmailSendCorreo', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'EmailSendCorreo', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'EmailSendCorreo', 'EscrituraLectura');
+
+-- 18 Grupos --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Grupos', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Grupos', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Grupos', 'EscrituraLectura');
+
+-- 19 Index --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Index', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Index', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Index', 'EscrituraLectura');
+
+-- 20 Mensualidades docente
+INSERT INTO tblPermisos VALUES(DEFAULT, 'MensualidadDocentes', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'MensualidadDocentes', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'MensualidadDocentes', 'EscrituraLectura');
+
+-- 21 Nota --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Nota', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Nota', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Nota', 'EscrituraLectura');
+
+-- 22 Pagos --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Pagos', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Pagos', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Pagos', 'EscrituraLectura');
+
+-- 23 Proyectos --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Proyectos', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Proyectos', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Proyectos', 'EscrituraLectura');
+
+-- 24 REstudiantes --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'REstudiantes', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'REstudiantes', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'REstudiantes', 'EscrituraLectura');
+
+-- 25 Registro --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Registro', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Registro', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Registro', 'EscrituraLectura');
+
+-- 26 Registro Colegio --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroColegio', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroColegio', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroColegio', 'EscrituraLectura');
+
+-- 27 Registro Docente --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroDocente', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroDocente', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'RegistroDocente', 'EscrituraLectura');
+
+-- 28 Verificar --
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Verificar', 'SoloEscritura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Verificar', 'SoloLectura');
+INSERT INTO tblPermisos VALUES(DEFAULT, 'Verificar', 'EscrituraLectura');
